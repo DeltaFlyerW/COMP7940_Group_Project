e762943a7d47e57c6db1a4dc577c4d38ad8c8e64
parker-int64 <huxiaochuan2011@hotmail.com>
2024-03-31 12:41:52 +0800

    Fixed some  error

--- service/MasterServer/src/util/projectRoot.py
+++ service/MasterServer/src/util/projectRoot.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-
+import logging
 projectRoot = Path(__file__)
 while "MasterServer" not in projectRoot.name:
     projectRoot = projectRoot.parent
+
+print("Project root %s", projectRoot)
